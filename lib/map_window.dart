c505459f7e1ad060e0d2f8a3323ca597a2af98ed
P. Hotz <p.hotz@posteo.net>
2017-08-24 23:50:43 +0200

    Lay some of the groundwork for a 3d map

--- lib/map_window.dart
+++ lib/map_window.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:html';
+import 'dart:web_gl';
 
 class Unselect {
   Unselect();
@@ -28,7 +29,7 @@ class MapWindow {
   Element _leftBumper;
   Element _rightBumper;
   Element _map;
-  double _zoomFactor = 10.0;
+  double _zoomFactor = 20.0;
   int _playerId;
   int _x = 0;
   int _y = 30;
@@ -37,9 +38,12 @@ class MapWindow {
   // the translation in world units applied when the player's
   // pointer touches the 'bumper'
   int _step = 3;
+  RenderingContext _gl;
 
-  MapWindow(this._playerId) {
-    _map = new DivElement()
+  MapWindow(int width, int height, this._playerId) {
+    _map = new CanvasElement()
+      ..width = width
+      ..height = height
       ..classes.add('map-window__map');
 
     _leftBumper = new DivElement()
@@ -59,6 +63,10 @@ class MapWindow {
       ..children.add(_rightBumper)
       ..onClick.listen(this._handleClick)
       ..onContextMenu.listen(this._handleContextMenu);
+
+    _gl = new RenderingContext(_map.getContext('webgl'))
+      ..clearColor(0.0, 0.0, 0.0, 0.0)
+      ..enable(DEPTH_TEST);
   }
 
   void _onTouchLeftBumper(ev) {
@@ -70,80 +78,20 @@ class MapWindow {
   }
 
   void _handleContextMenu(ev) {
-    if (ev.target.classes.contains('map-window__entity')) {
-      int entityId = int.parse(ev.target.dataset['entity-id']);
-      var selectTargetEvent = new SelectTarget(entityId);
-      _streamController.add(selectTargetEvent);
-    }
-    else {
-      // determine coordinates relative to the map
-      _map.style.display = '';
-      var rec = _map.getBoundingClientRect();
-      double x = ev.client.x / _zoomFactor - _x;
-      double y = -(ev.client.y - rec.top) / _zoomFactor + _y;
-
-      var newEv = new SelectPosition(x, y);
-      _streamController.add(newEv);
-    }
 
     // prevent the context menu from showing up
     ev.preventDefault();
   }
 
   void _handleClick(ev) {
-    if (ev.target.classes.contains('map-window__entity')) {
-      int entityId = int.parse(ev.target.dataset['entityId']);
-
-      _streamController.add(new EntitySelect(entityId));      
-    }
-    else {
-      _streamController.add(new Unselect());
-    }
   }
 
   void rerender(state) {
-    _map.children.clear();
+    _gl.clear(COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT);
 
     state['point_masses'].forEach((entityId, pointMass) {
-      Element entityEl = new DivElement();
-
-      entityEl.classes.add('map-window__entity');
-
-      entityEl.dataset['entityId'] = entityId.toString();
-
-      if (state.containsKey('resources') &&
-          state['resources'].containsKey(entityId)) {
-        entityEl.classes.add('map-window__entity--gold-mine');
-      }
-
-      if (state.containsKey('commands') &&
-          state['commands'].containsKey(entityId)) {
-        entityEl.classes.add('map-window__entity--unit');
-      }
-
-      if (state.containsKey('unit_factories') &&
-          state['unit_factories'].containsKey(entityId)) {
-        entityEl.classes.add('map-window__entity--unit-factory');
-      }
-
-      if (state.containsKey('ownership') &&
-          state['ownership'].containsKey(entityId.toString())) {
-        int ownerId = state['ownership'][entityId.toString()];
-
-        if (ownerId == _playerId) {
-          entityEl.classes.add('map-window__entity--friendly');
-        }
-      }
       
-      var xWorld = pointMass['position']['x'];
-      var yWorld = pointMass['position']['y'];
-
-      var xScreen = (xWorld + _x) * _zoomFactor;
-      var yScreen = (_y - yWorld) * _zoomFactor;
-
-      entityEl.style.transform = 'translate3d(${xScreen}px,${yScreen}px,0)';
-
-      _map.children.add(entityEl);
+      
     });
   }
 }
